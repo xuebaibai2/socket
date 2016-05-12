9aa647cf8d7e29867458918c047ea28937ed5cc0
Yanfeng Li <xuebaibai2@hotmail.com>
2016-05-12 18:51:15 +1000

    Initiate Project

---
+++ server.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Cayden on 16/5/12.
+ */
+var PORT = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+app.use(express.static(__dirname + "/public"));
+
+http.listen(PORT, function () {
+    console.log("Server Started at Port: " + PORT);
+});
