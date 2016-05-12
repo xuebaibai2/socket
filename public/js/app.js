126d3623628f7bf06df1e5916f1456bce2640fac
Yanfeng Li <xuebaibai2@hotmail.com>
2016-05-13 01:49:33 +1000

    Add front add form submit message

--- public/js/app.js
+++ public/js/app.js
@@ -12,3 +12,18 @@ socket.on('message', function (message) {
     console.log("New Message");
     console.log(message.text);
 });
+
+//handles submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit',function (event) {
+    event.preventDefault();
+
+    var $message = $form.find('input[name=message]');
+
+    socket.emit('message',{
+        text: $message.val()
+    });
+    $message.val('');
+    $message.focus();
+});
