7bd9cfd1f0c76a104aca67d64efd252d2e09501a
Yanfeng Li <xuebaibai2@hotmail.com>
2016-05-12 19:40:53 +1000

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,8 @@ var socket = io();
 socket.on('connect',function () {
     console.log('connected to socket.io server');
 });
+
+socket.on('message', function (message) {
+    console.log("New Message");
+    console.log(message.text);
+});
