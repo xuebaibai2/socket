f2a8b967cd201b6454b2021bc76323ac98a907ff
Yanfeng Li <xuebaibai2@hotmail.com>
2016-05-13 16:36:48 +1000

    add name support'

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,12 @@
  *
  * Created by Cayden on 16/5/12.
  */
+
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
 var socket = io();
+
+console.log(name + ' wants to join ' + room);
 
 socket.on('connect', function () {
     console.log('connected to socket.io server');
@@ -12,7 +17,11 @@ socket.on('message', function (message) {
     console.log("New Message");
     console.log(message.text);
     var momentTimestamp = moment.utc(message.timestamp);
-    jQuery('.messages').append('<p><strong>'+momentTimestamp.local().format('MMM-Do-YYYY h:mm:ssa')+'</strong>: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + message.text + '</p>');
+    var $message = jQuery('.messages');
+
+    $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('MMM-Do-YYYY h:mm:ssa') + '</strong></p>');
+    $message.append('<p>' + message.text + '</p>');
+    // jQuery('.messages').append('<p><strong>'+momentTimestamp.local().format('MMM-Do-YYYY h:mm:ssa')+'</strong>: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + message.text + '</p>');
 });
 
 //handles submitting of new message
@@ -24,6 +33,7 @@ $form.on('submit', function (event) {
     var $message = $form.find('input[name=message]');
 
     socket.emit('message', {
+        name: name,
         text: $message.val()
     });
     $message.val('');
